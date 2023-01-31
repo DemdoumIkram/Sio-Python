ee58716655e59c3650274a24719c86605787de64
DemdoumIkram <ikram.ik33@gmail.com>
2023-01-31 09:30:35 +0100

    Add : upload_pdf route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from PyPDF2 import PdfReader
+
 app = Flask(__name__)
+
+documents = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/documents", methods=["POST"])
+def upload_pdf():
+    file = request.files["file"]
+
+    if not file:
+        return "Error readingthe file"
+
+    if file.filename.split(".")[1] != "pdf":
+        return "Not a PDF"
+
+    reader = PdfReader(file)
+
+    text = ""
+
+    for page in reader.pages:
+        text += page.extract_text()
+
+    document_id = len(documents)
+    documents[document_id] = {
+        "text": text,
+        "meta": reader.metadata
+    }
+    return documents[document_id]
